176a13769b1805821102873566a248009f890376
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-07-30 11:10:44 +0200

    Create basic files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import routes from './routes';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={store}>
+       <Router history={hashHistory} routes={routes} />
+    </Provider>,
+    rootElement
+);
+
